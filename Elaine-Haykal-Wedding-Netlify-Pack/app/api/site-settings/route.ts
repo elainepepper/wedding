33db699676dd0117585364f96fc7ba87d099b464
elainepepper <2elaine4u@gmail.com>
2026-08-02 00:14:18 +0800

    Delete Elaine-Haykal-Wedding-Netlify-Pack directory

--- Elaine-Haykal-Wedding-Netlify-Pack/app/api/site-settings/route.ts
+++
@@ -1,7 +0,0 @@
-import { weddingRef } from "../../../lib/firebase-admin";
-
-export async function GET() {
-  const snapshot = await weddingRef.get();
-  const settings = snapshot.data() ?? {};
-  return Response.json({ musicUrl: settings.music_url ?? null, musicTitle: settings.music_title ?? null });
-}
